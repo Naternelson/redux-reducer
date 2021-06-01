374682479950809d33dc8391d787d51cdb962603
Nate Nelson <7nater.nelson@gmail.com>
2021-06-01 13:27:49 -0600

    commit tree f5ddd6b2faa2fb7b7557e85c6825c60beba40f8f

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
 export function manageFriends(state, action){
+    const {friend} = action
+    const {friends} = state 
+    switch(action.type){
+        case 'ADD_FRIEND':
+            friends.push(friend)
+            return {friends} 
+        case 'REMOVE_FRIEND': 
+            const index = friends.indexOf(friend)
+            friends.splice(index,1)
+            return {friends}
+        default:
+            return state 
+    }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
 export function managePresents(state, action){
+    switch(action.type){
+        case "INCREASE":
+            return {numberOfPresents: state.numberOfPresents + 1}
+        case "DECREASE":
+            return {numberOfPresents: state.numberOfPresents > 0 ? state.numberOfPresents -1 : 0 }
+        default: 
+            return state
+    }
 }
